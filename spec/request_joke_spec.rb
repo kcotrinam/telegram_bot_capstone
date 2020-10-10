426d950de4c961178d5393e15d4c9f7f7061dfc2
Kevin Cotrina <kcotrinam92@gmail.com>
2020-10-09 23:03:28 -0500

    Refactor test description

--- spec/request_joke_spec.rb
+++ spec/request_joke_spec.rb
@@ -3,13 +3,13 @@ require_relative '../lib/request_joke'
 describe JokeRequest do
   let(:new_joke) { JokeRequest.new }
   context '#joke' do
-    it 'When joke mthod is called, returns a String' do
+    it 'When joke method is called, returns a String' do
       expect(new_joke.joke('dev')).to be_a String
     end
   end
 
   context '#random_joke' do
-    it 'When joke mthod is called, returns a String' do
+    it 'When random_joke method is called, It returns a string' do
       expect(new_joke.random_joke).to be_a String
     end
   end
