af0d3dc81c5b15d36f58d23436c23de786ade768
Kevin Cotrina <kcotrinam92@gmail.com>
2020-10-11 04:25:14 -0500

    Add environment variable for telegram api token

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'dotenv'
 gem 'rest-client', '~> 2.1'
 gem 'rspec', '~> 3.9'
 gem 'telegram-bot-ruby'
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,20 +1,23 @@
-# rubocop: disable Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/AbcSize,Layout/LineLength
-
 require 'telegram/bot'
 require_relative '../lib/request_joke.rb'
+require 'dotenv/load'
 
 class Bot
   attr_reader :token
   attr_reader :category
 
   def initialize
-    @token = '1152730733:AAEr8ZjNR6PNWum0jXRetvdDQxWk5JsOsLQ'
+    @token = ENV['TELEGRAM_API_KEY']
     @category = JokeRequest.new
     begin
       start_bot
     rescue Telegram::Bot::Exceptions::ResponseError => e
       puts "Bot not connecting properly. Presenting: #{e}"
     end
+  end
+
+  def instruction
+    'Use /start to start the bot, /stop to stop the bot, You can choose beetwing these categories: /dev, /history, /political, /celebrity, /animal, /science /random'
   end
 
   private
@@ -25,17 +28,9 @@ class Bot
         case message.text
         when '/start'
           @category.random_joke
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, Welcome to chuckjoke. This bot will show you a Chuck Norris joke based on the category you choose. Use /start to start the bot, /stop to stop the bot, You can choose beetwing these categories: /dev, /history, /political, /celebrity, /animal, /science /random")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, Welcome to chuckjoke. #{instruction}")
         when '/dev'
           bot.api.send_message(chat_id: message.chat.id, text: @category.joke('dev'), date: message.date)
-        when '/history'
-          bot.api.send_message(chat_id: message.chat.id, text: @category.joke('history'), date: message.date)
-        when '/political'
-          bot.api.send_message(chat_id: message.chat.id, text: @category.joke('political'), date: message.date)
-        when '/celebrity'
-          bot.api.send_message(chat_id: message.chat.id, text: @category.joke('celebrity'), date: message.date)
-        when '/animal'
-          bot.api.send_message(chat_id: message.chat.id, text: @category.joke('animal'), date: message.date)
         when '/science'
           bot.api.send_message(chat_id: message.chat.id, text: @category.joke('science'), date: message.date)
         when '/random'
@@ -43,11 +38,9 @@ class Bot
         when '/stop'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
         else
-          bot.api.send_message(chat_id: message.chat.id, text: "Wrong entry, #{message.from.first_name}, please choose one of these options:/start, /dev, /history, /political, /celebrity, /animal, /science, /random, /stop")
+          bot.api.send_message(chat_id: message.chat.id, text: "Wrong entry, #{message.from.first_name}, please, #{instruction}")
         end
       end
     end
   end
 end
-
-# rubocop: enable Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/AbcSize,Layout/LineLength
