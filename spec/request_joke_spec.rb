4bc9704e6e6927f6e5e007582ef39b0c6e580367
Kevin Cotrina <kcotrinam92@gmail.com>
2020-10-12 11:21:12 -0500

    Add test cases for rspec

--- spec/request_joke_spec.rb
+++ spec/request_joke_spec.rb
@@ -6,11 +6,25 @@ describe JokeRequest do
     it 'When joke method is called, returns a String' do
       expect(new_joke.joke('dev')).to be_a String
     end
+
+    it 'When joke method is called, It does not return a Number' do
+      expect(new_joke.joke('dev')).not_to be_a Numeric
+    end
+
+    it 'When joke method is called, It does not return false' do
+      expect(new_joke.joke('dev')).not_to eql(false)
+    end
   end
 
   context '#random_joke' do
     it 'When random_joke method is called, It returns a string' do
       expect(new_joke.random_joke).to be_a String
+    end
+    it 'When random_joke method is called, It does not return an array' do
+      expect(new_joke.random_joke).not_to be_a Array
+    end
+    it 'When random_joke method is called, It does not return true' do
+      expect(new_joke.random_joke).not_to be true
     end
   end
 end
